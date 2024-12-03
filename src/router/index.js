7e0d264858e3dad5d993917b7ee52a1d1936fbc3
lomejor77 <lomejor77@gmail.com>
2024-12-03 19:05:13 -0300

    Se agregan nuevas rutas necesarias

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AvesView.vue')
-  }
+  },
+  {
+    path: '/aves/:id',
+    name: 'ave-detalle',
+    
+    component: () => import(/* webpackChunkName: "about" */ '../views/AveDetalle.vue')
+  },
+  { path: '/:pathMatch(.*)*', name: 'not', component: NotFoundView },
 ]
 
 const router = createRouter({
---
+++ src/views/AveDetalle.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>VISTA AVE DETALLE</h1>
+    </div>
+</template>
